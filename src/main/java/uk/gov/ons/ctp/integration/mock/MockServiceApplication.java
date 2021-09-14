2d6aa74e29f51a942f136f197c38f500f3f4247b
Rob Catling <github@rpc-soft.com>
2021-09-14 11:54:10 +0100

    SOCINT-57 removed redundant pom dependencies/plugins. Renamed main class

--- src/main/java/uk/gov/ons/ctp/integration/mock/MockServiceApplication.java
+++ src/main/java/uk/gov/ons/ctp/integration/mock/MockServiceApplication.java
@@ -9,49 +9,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpStatus;
 import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.common.error.RestExceptionHandler;
-import uk.gov.ons.ctp.common.jackson.CustomObjectMapper;
 import uk.gov.ons.ctp.common.rest.RestClient;
 import uk.gov.ons.ctp.common.rest.RestClientConfig;
 import uk.gov.ons.ctp.integration.mock.client.MockServiceClient;
 import uk.gov.ons.ctp.integration.mock.config.AppConfig;
 
-/** The 'main' entry point for the Mock AI SpringBoot Application. */
+/** The 'main' entry point for the Mock Service SpringBoot Application. */
 @Slf4j
 @SpringBootApplication
 @EnableCaching
-public class MockApplication {
+public class MockServiceApplication {
   @Autowired private AppConfig appConfig;
 
-  /**
-   * The main entry point for this application.
-   *
-   * @param args runtime command line args
-   */
   public static void main(final String[] args) {
-
-    SpringApplication.run(MockApplication.class, args);
+    SpringApplication.run(MockServiceApplication.class, args);
   }
 
-  /**
-   * Custom Object Mapper
-   *
-   * @return a customer object mapper
-   */
-  @Bean
-  @Primary
-  public CustomObjectMapper customObjectMapper() {
-    return new CustomObjectMapper();
-  }
-
-  /**
-   * Bean used to map exceptions for endpoints
-   *
-   * @return the service client
-   */
   @Bean
   public RestExceptionHandler restExceptionHandler() {
     return new RestExceptionHandler();
